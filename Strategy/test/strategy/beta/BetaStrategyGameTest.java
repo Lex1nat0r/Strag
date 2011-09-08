39a9764cb740b6d13064f97a0a32dc88b1ad9869
Alex Thornton-Clark <alex.thornton.clark@gmail.com>
2011-09-08 16:32:28 -0400

    Added test skeletons for public methods in BetaStrategyGame.java
    Contributors: Andrew, TC, Gabe

---
+++ Strategy/test/strategy/beta/BetaStrategyGameTest.java
@@ -0,0 +1,53 @@
+package strategy.beta;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test case for the BetaStrategy game.
+ * 
+ * @author Andrew Hurle, Alex Thornton-Clark, Gabriel Stern-Robbins
+ * @version Sep 8, 2011
+ */
+public class BetaStrategyGameTest {
+
+	private BetaStrategyGame game;
+	
+	@Before
+	public void setUp() throws Exception {
+		game = new BetaStrategyGame();
+	}
+
+	@Test
+	public void testBetaStrategyGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitializeGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsGameOver() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWinner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPieceAt() {
+		fail("Not yet implemented");
+	}
+
+}
