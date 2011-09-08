ecb7b926650e939a4424b83c5cdfa7b79b3e14ef
Alex Thornton-Clark <alex.thornton.clark@gmail.com>
2011-09-08 16:38:12 -0400

    Added RectangularStrategyBoardTest.java
    Contributors: Gabe, TC, Andrew

---
+++ Strategy/test/strategy/common/RectangularStrategyBoardTest.java
@@ -0,0 +1,57 @@
+package strategy.common;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RectangularStrategyBoardTest {
+
+	private RectangularStrategyBoard board;
+	
+	@Before
+	public void setUp() throws Exception {
+		board = new RectangularStrategyBoard(6, 6);
+	}
+
+	@Test
+	public void testRectangularStrategyBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIterator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPieceAt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPutPieceAt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsOccupied() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDistance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitializeBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
