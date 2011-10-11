11d3efee02815a35c336ba9180ab1990365c1140
Alex Thornton-Clark <alex.thornton.clark@gmail.com>
2011-10-11 19:36:52 -0400

    File comment in BattleResult

--- Strategy/src/strategy/playeratcahgsr/common/BattleResult.java
+++ Strategy/src/strategy/playeratcahgsr/common/BattleResult.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * This file is used in CS4233, Object-oriented Analysis and Design
+ * 
+ * Copyright (c) 2011 Worcester Polytechnic Institute. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Author: Alex Thornton-Clark, Andrew Hurle, Gabriel Stern-Robbins
+ *******************************************************************************/
 package strategy.playeratcahgsr.common;
 
 /**
